46cfd97f4c9493f72334c683dba50c61503039a1
Drektion <wenq93020@gmail.com>
2022-04-08 22:22:53 +0800

    2022-4-8

---
+++ 2022-4-8/ReturnDemo.java
@@ -0,0 +1,1 @@
+/*return有两个基本用途	1、返回方法的返回值	2、终止当前程序*/public class ReturnDemo{		public static void main(String[] args){				System.out.println(get());		for(int i = 0;i < 10;i++){			System.out.println(i);			if(i == 5){				return;				// System.exit(-1);			}			System.out.println("接着执行");		}			}		public static int get(){		return 100;	}	}
